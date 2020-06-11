8f368ad31afcb24ab1a1564cac817032ad49916c
Dorin <dorin.fodor@gmail.com>
2020-06-11 23:05:05 +0100

    [JS] Refactored interpreter tests

--- tests/tst_interpreter.h
+++ tests/tst_interpreter.h
@@ -11,177 +11,47 @@
 
 using namespace testing;
 
-TEST(Interpreter, InterpretExpression)
+struct test {
+    std::string code;
+    js::Value::Type result_type;
+    std::string result_value;
+    test(std::string c, js::Value::Type r_t, std::string r_v)
+        : code(c), result_type(r_t), result_value(r_v) {}
+};
+
+std::vector<test> tests = {
+    test("1+2", js::Value::NUMBER, "3"),
+    test("2*3;", js::Value::NUMBER, "6"),
+    test("2 * (3 + 1);", js::Value::NUMBER, "8"),
+    test("\"hello\" + \"world\"", js::Value::STRING, "helloworld"),
+    test("\"hello\" + 100", js::Value::STRING, "hello100"),
+    test("2 * 2; 3 * 3", js::Value::NUMBER, "9"),
+    test("var x = 1", js::Value::UNDEFINED, ""),
+    test("var x = 1; x", js::Value::NUMBER, "1"),
+    test("var x = 10 * (3+3); x + 1;", js::Value::NUMBER, "61"),
+    test("var x = 10 * (3+3); var y = x + 1; y-1;", js::Value::NUMBER, "60"),
+};
+
+TEST(Interpreter, AllTests)
 {
-    js::Tokenizer t = js::Tokenizer("1+2");
-    auto p = js::Parser(t);
-    js::Program* program = p.parse_program();
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
+    for (test test : tests) {
+        js::Tokenizer t = js::Tokenizer(test.code);
+        auto p = js::Parser(t);
+        js::Program* program;
+        try {
+            program = p.parse_program();
+        } catch (js::SyntaxError e) {
+            std::cerr << "Parsing error: " + e.message << std::endl;
+        }
+        js::Interpreter interpreter;
+        auto result = interpreter.run(program);
 
-    program->dump(0);
+        program->dump(0);
+        std::cout << std::endl;
 
-    EXPECT_EQ(result.get_value(), "3");
-}
-
-TEST(Interpreter, InterpretExpression2)
-{
-    js::Tokenizer t = js::Tokenizer("2*3;");
-    auto p = js::Parser(t);
-    js::Program* program = p.parse_program();
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    program->dump(0);
-
-    EXPECT_EQ(result.get_value(), "6");
-}
-
-TEST(Interpreter, InterpretExpression3)
-{
-    js::Tokenizer t = js::Tokenizer("2 * (3 + 1);");
-    auto p = js::Parser(t);
-    js::Program* program;
-    try {
-        program = p.parse_program();
-    } catch (js::SyntaxError e) {
-        std::cerr << "Parsing error: " + e.message << std::endl;
+        EXPECT_EQ(result.get_type(), test.result_type);
+        EXPECT_EQ(result.get_value(), test.result_value);
     }
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    program->dump(0);
-
-    EXPECT_EQ(result.get_value(), "8");
-}
-
-TEST(Interpreter, InterpretExpression4)
-{
-    js::Tokenizer t = js::Tokenizer("\"hello\" + \"world\"");
-    auto p = js::Parser(t);
-    js::Program* program;
-    try {
-        program = p.parse_program();
-    } catch (js::SyntaxError e) {
-        std::cerr << "Parsing error: " + e.message << std::endl;
-    }
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    program->dump(0);
-
-    EXPECT_EQ(result.get_value(), "helloworld");
-}
-
-TEST(Interpreter, InterpretExpression5)
-{
-    js::Tokenizer t = js::Tokenizer("\"hello\" + 100");
-    auto p = js::Parser(t);
-    js::Program* program;
-    try {
-        program = p.parse_program();
-    } catch (js::SyntaxError e) {
-        std::cerr << "Parsing error: " + e.message << std::endl;
-    }
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    program->dump(0);
-
-    EXPECT_EQ(result.get_value(), "hello100");
-}
-
-TEST(Interpreter, InterpretExpression6)
-{
-    js::Tokenizer t = js::Tokenizer("2 * 2; 3 * 3");
-    auto p = js::Parser(t);
-    js::Program* program;
-    try {
-        program = p.parse_program();
-    } catch (js::SyntaxError e) {
-        std::cerr << "Parsing error: " + e.message << std::endl;
-    }
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    program->dump(0);
-
-    EXPECT_EQ(result.get_value(), "9");
-}
-
-TEST(Interpreter, InterpretExpression7)
-{
-    js::Tokenizer t = js::Tokenizer("var x = 1");
-    auto p = js::Parser(t);
-    js::Program* program;
-    try {
-        program = p.parse_program();
-    } catch (js::SyntaxError e) {
-        std::cerr << "Parsing error: " + e.message << std::endl;
-    }
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    program->dump(0);
-
-    EXPECT_EQ(result.get_type(), js::Value::UNDEFINED);
-}
-
-TEST(Interpreter, InterpretExpression8)
-{
-    js::Tokenizer t = js::Tokenizer("var x = 1; x");
-    auto p = js::Parser(t);
-    js::Program* program;
-    try {
-        program = p.parse_program();
-    } catch (js::SyntaxError e) {
-        std::cerr << "Parsing error: " + e.message << std::endl;
-    }
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    program->dump(0);
-
-    EXPECT_EQ(result.get_type(), js::Value::NUMBER);
-    EXPECT_EQ(result.get_value(), "1");
-}
-
-TEST(Interpreter, InterpretExpression9)
-{
-    js::Tokenizer t = js::Tokenizer("var x = 10 * (3+3); x + 1;");
-    auto p = js::Parser(t);
-    js::Program* program;
-    try {
-        program = p.parse_program();
-    } catch (js::SyntaxError e) {
-        std::cerr << "Parsing error: " + e.message << std::endl;
-    }
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    program->dump(0);
-
-    EXPECT_EQ(result.get_type(), js::Value::NUMBER);
-    EXPECT_EQ(result.get_value(), "61");
-}
-
-TEST(Interpreter, InterpretExpression10)
-{
-    js::Tokenizer t = js::Tokenizer("var x = 10 * (3+3); var y = x + 1; y-1;");
-    auto p = js::Parser(t);
-    js::Program* program;
-    try {
-        program = p.parse_program();
-    } catch (js::SyntaxError e) {
-        std::cerr << "Parsing error: " + e.message << std::endl;
-    }
-    js::Interpreter interpreter;
-    auto result = interpreter.run(program);
-
-    program->dump(0);
-
-    EXPECT_EQ(result.get_type(), js::Value::NUMBER);
-    EXPECT_EQ(result.get_value(), "60");
 }
 
 #endif // TST_INTERPRETER_H
